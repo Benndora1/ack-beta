04d302175b4d26865ffc31739febdcc6c8b61fb9
benndora1 <benndora2@gmail.com>
2021-04-06 15:35:01 +0300

    changes

--- src/views/theme/colors/Colors.js
+++ src/views/theme/colors/Colors.js
@@ -5,6 +5,18 @@ import {
   CCardBody,
   CCardHeader,
   CCol,
+  CSwitch,
+  CInputCheckbox,
+  CInputRadio,
+  CInputFile,
+  CCardFooter,
+  CSelect,
+  CTextarea,
+  CFormText,
+  CForm,
+  CLabel,
+  CInput,
+  CFormGroup,
   CModal,
   CModalBody,
   CModalFooter,
@@ -35,21 +47,114 @@ const Table = () => {
               onClose={setModal}
             >
               <CModalHeader closeButton>
-                <CModalTitle>Modal title</CModalTitle>
+                <CModalTitle>Member Details</CModalTitle>
               </CModalHeader>
               <CModalBody>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
-              </CModalBody>
+              <CCard>
+            {/* <CCardHeader>
+              Member Details
+            </CCardHeader> */}
+            <CCardBody>
+              <CForm action="" method="post" encType="multipart/form-data" className="form-horizontal">
+                
+                <CFormGroup row>
+                  <CCol md="3">
+                    <CLabel htmlFor="text-input">First Name</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CInput id="text-input" name="text-input" placeholder="First Name" />
+                  </CCol>
+                </CFormGroup>
+                <CFormGroup row>
+                  <CCol md="3">
+                    <CLabel htmlFor="text-input">Last Name</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CInput id="text-input" name="text-input" placeholder="Last Name" />
+                  </CCol>
+                </CFormGroup>
+                <CFormGroup row>
+                  <CCol md="3">
+                    <CLabel htmlFor="number-input">Id Number</CLabel>
+                    </CCol>
+                    <CCol xs="12" md="9">
+                      <CInput id="input-number" name="number-input" placeholder="Id Number"></CInput>
+                    </CCol>
+                  </CFormGroup>
+
+                  <CFormGroup row>
+                    <CCol md="3">
+                      <CLabel htmlFor="phone-number">Phone Number</CLabel>
+                    </CCol>
+                    <CCol xs="12" md="9">
+                      <CInput id="input-number" name="number-input" placeholder="0700000000"></CInput>
+                    </CCol>
+                  </CFormGroup>
+                  <CFormGroup row>
+                    <CCol md="3">
+                      <CLabel htmlFor="phone-number">Phone Number</CLabel>
+                    </CCol>
+                    <CCol xs="12" md="9">
+                      <CInput id="input-number" name="number-input" placeholder="0700000000"></CInput>
+                    </CCol>
+                  </CFormGroup>
+                  <CFormGroup row>
+                    <CCol md="3">
+                      <CLabel htmlFor="phone-number">NHIF Number</CLabel>
+                    </CCol>
+                    <CCol xs="12" md="9">
+                      <CInput id="input-number" name="number-input" placeholder="NHIF Number"></CInput>
+                    </CCol>
+                  </CFormGroup>
+                  <CFormGroup row>
+                    <CCol md="3">
+                      <CLabel htmlFor="phone-number">YoB</CLabel>
+                    </CCol>
+                    <CCol xs="12" md="9">
+                      <CInput id="input-number" name="number-input" placeholder="1900"></CInput>
+                    </CCol>
+                  </CFormGroup>
+                  <CFormGroup row>
+                    <CCol md="3">
+                      <CLabel htmlFor="phone-number">Card Number</CLabel>
+                    </CCol>
+                    <CCol xs="12" md="9">
+                      <CInput id="input-number" name="number-input" placeholder="Card-Number"></CInput>
+                    </CCol>
+                  </CFormGroup>
+                  <CFormGroup row>
+                  <CCol md="3">
+                    <CLabel htmlFor="select">Rank</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CSelect custom name="select" id="select">
+                      <option value="0">Please select Rank</option>
+                      <option value="1">Bishop</option>
+                      <option value="2">Archdeacons</option>
+                      <option value="3">Area Deans</option>             
+                      <option value="1">Clergy</option>
+                      <option value="1">Office Staffs</option>
+                      <option value="1">Chaplains</option>
+                      <option value="1">Evangelists</option>
+                    </CSelect>
+                  </CCol>
+                </CFormGroup>
+              </CForm>
+            </CCardBody>
+            {/* <CCardFooter>
+              <CButton type="submit" size="sm" color="primary"><CIcon name="cil-scrubber" /> Submit</CButton>
+              <CButton type="reset" size="sm" color="danger"><CIcon name="cil-ban" /> Reset</CButton>
+            </CCardFooter> */}
+          </CCard>
+                </CModalBody>
               <CModalFooter>
-                <CButton color="primary">Do Something</CButton>{' '}
+                {/* <CButton color="primary">Do Something</CButton>{' '}
                 <CButton 
                   color="secondary" 
                   onClick={() => setModal(false)}
-                >Cancel</CButton>
+                >Cancel</CButton> */}
+                <CButton type="submit" size="sm" color="primary"><CIcon name="cil-scrubber" /> Submit</CButton>
+              <CButton type="reset" size="sm" color="danger" onClick={() => setModal(false)}><CIcon name="cil-ban" /> Close</CButton>
               </CModalFooter>
             </CModal>
 
@@ -57,7 +162,8 @@ const Table = () => {
               <thead className="thead-light">
                 <tr>
                   <th className="text-center"><CIcon name="cil-people" /></th>
-                  <th>Member</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
                   <th>DoB</th>
                   <th>IdNo</th>
                   <th>Phone No</th>
